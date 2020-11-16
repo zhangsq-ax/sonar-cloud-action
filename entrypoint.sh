67f81e00ccf1a472d19f566e1dcb24669d96e40e
zhangsq <zhangsq@mapbar.com>
2020-11-16 18:21:32 +0800

    test: 输出 PATH 变量

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,7 @@ if [[ -z "${SONARCLOUD_URL}" ]]; then
   SONARCLOUD_URL="https://sonarcloud.io"
 fi
 
+echo $PATH
 build-wrapper-linux-x86-64 --out-dir ${BUILD_WRAPPER_OUTPUT_DIR} ${INPUT_BUILDCOMMAND}
 
 #sonar-scanner -Dsonar.projectBaseDir=${POROJECT_ROOT_DIR} -Dsonar.host.url=${SONARCLOUD_URL} ${INPUT_ARGS}
